8489f38dc60dc315085927bbbe8186971708b20b
Matus Bursa <burso@burso.eu>
2023-03-24 12:30:49 +0100

    changed dashboard

--- streamlit/f1.py
+++ streamlit/f1.py
@@ -1,12 +1,9 @@
 import streamlit as st
-import numpy as np
-import time
-import pandas as pd
 
 import matplotlib.pyplot as plt
 import fastf1.plotting
-from fastf1 import plotting
-from fastf1 import utils
+import pandas as pd
+from fastf1 import plotting, utils
 
 fastf1.Cache.enable_cache('/tmp')  # replace with your cache directory
 
@@ -27,6 +24,9 @@ def load_q(session, telX, telY, lblX, lblY, dD, dO, dT):
     ham_tel = ham_lap.get_car_data().add_distance()
 
     fig, ax = plt.subplots()
+    if driver_dict[driverOne][1] == driver_dict[driverTwo][1]:
+        driver_dict[driverTwo][1] = "FFFFFF"
+
     ax.plot(ver_tel[telX], ver_tel[telY], color='#'+driver_dict[driverOne][1], label=driver_dict[driverOne][0])
     ax.plot(ham_tel[telX], ham_tel[telY], color='#'+driver_dict[driverTwo][1], label=driver_dict[driverTwo][0])
 
@@ -39,11 +39,12 @@ def load_q(session, telX, telY, lblX, lblY, dD, dO, dT):
     
 
 def draw_chart(session):
-    session.load()
+    session.load(telemetry=True, weather=True)
     driver_dict = {}
 
     for d in session.drivers:
         driver = session.get_driver(d)
+        #print(driver)
         driver_dict[driver['FullName']] = [driver['Abbreviation'], driver['TeamColor']]
 
     col1, col2 = st.columns(2)
@@ -53,33 +54,58 @@ def draw_chart(session):
     with col2:
         driverTwo = st.selectbox('Select driver #2', driver_dict.keys())
 
-    col1, col2 = st.columns(2)
-    with col1:
-        st.header("Gear")
-        fig = load_q(session, telX="Distance", telY="nGear", lblX="Distance in m", lblY="Gear",dD=driver_dict, dO=driverOne, dT=driverTwo)
-        st.plotly_chart(fig, use_container_width=True)
+    st.header("Speed")
+    fig = load_q(session, telX="Distance", telY="Speed", lblX="Distance in m", lblY="Speed in km/h", dD=driver_dict, dO=driverOne, dT=driverTwo)
+    st.plotly_chart(fig, use_container_width=True)
 
-    with col2:
-        st.header("RPM")
-        fig = load_q(session, telX="Distance", telY="RPM", lblX="Distance in m", lblY="RPM",dD=driver_dict, dO=driverOne, dT=driverTwo)
-        st.plotly_chart(fig, use_container_width=True)
+    st.header("Gear")
+    fig = load_q(session, telX="Distance", telY="nGear", lblX="Distance in m", lblY="Gear",dD=driver_dict, dO=driverOne, dT=driverTwo)
+    st.plotly_chart(fig, use_container_width=True)
 
-    with col1:
-        st.header("Speed")
-        fig = load_q(session, telX="Distance", telY="Speed", lblX="Distance in m", lblY="Speed in km/h", dD=driver_dict, dO=driverOne, dT=driverTwo)
-        st.plotly_chart(fig, use_container_width=True)
+    st.header("RPM")
+    fig = load_q(session, telX="Distance", telY="RPM", lblX="Distance in m", lblY="RPM",dD=driver_dict, dO=driverOne, dT=driverTwo)
+    st.plotly_chart(fig, use_container_width=True)
+
+    st.header("Throttle")
+    fig = load_q(session, telX="Distance", telY="Throttle", lblX="Distance in m", lblY="Throttle",dD=driver_dict, dO=driverOne, dT=driverTwo)
+    st.plotly_chart(fig, use_container_width=True)
+
+    st.header("Brake")
+    fig = load_q(session, telX="Distance", telY="Brake", lblX="Distance in m", lblY="Brake",dD=driver_dict, dO=driverOne, dT=driverTwo)
+    st.plotly_chart(fig, use_container_width=True)
+
+    st.header("DRS")
+    fig = load_q(session, telX="Distance", telY="DRS", lblX="Distance in m", lblY="DRS",dD=driver_dict, dO=driverOne, dT=driverTwo)
+    st.plotly_chart(fig, use_container_width=True)
+
+
 
 
 if __name__ == '__main__':
-    st.set_page_config(layout="wide")
-    tab1, tab2 = st.tabs(["Bahrain", "Jeddah"])
+    st.set_page_config(page_title="F1 Analysis (by matusso)", layout="wide")
 
-    with tab1:
-        st.header("Bahrain Grand Prix")
-        draw_chart(fastf1.get_session(2023, 'Bahrain Grand Prix', 'Q'))
+    col1, col2 = st.columns(2)
+    with col1:
+        selectedYear = st.selectbox('Year', [2018, 2019, 2020, 2021, 2022, 2023])
 
-    with tab2:
-        st.header("Jeddah Grand Prix")
-        draw_chart(fastf1.get_session(2023, 'Jeddah Grand Prix', 'Q'))
+    schedule = fastf1.get_event_schedule(selectedYear)
+    round_dict = {}
+    for index, row in schedule.iterrows():
+        if row["RoundNumber"] > 0:
+            round_dict[row["Location"]] = row["RoundNumber"] 
+    
+    with col2:
+        circuitName = st.selectbox('Select Circuit', round_dict.keys())
+
+    print(schedule.get_event_by_round(round_dict[circuitName]))
+    st.header(str(selectedYear) + "/" + circuitName)
+
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(["Practice 1", "Practice 2", "Practice 3", "Qualifying", "Race"])
+
+    with tab4:
+        draw_chart(fastf1.get_session(selectedYear, circuitName, 'Q'))
+
+#with tab5:
+        #race_results(fastf1.get_session(selectedYear, circuitName, 'R'))
 
     
