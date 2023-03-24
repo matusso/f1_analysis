06a822608c1d07beed2547a4d9fe69c3d7f6447e
Matus Bursa <burso@burso.eu>
2023-03-24 12:59:38 +0100

    added sidebar

--- streamlit/f1.py
+++ streamlit/f1.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import fastf1.plotting
 import pandas as pd
 from fastf1 import plotting, utils
+import sys
 
 fastf1.Cache.enable_cache('/tmp')  # replace with your cache directory
 
@@ -39,67 +40,71 @@ def load_q(session, telX, telY, lblX, lblY, dD, dO, dT):
     
 
 def draw_chart(session):
-    session.load(telemetry=True, weather=True)
-    driver_dict = {}
+    try:
+        session.load(telemetry=True, weather=True)
+        driver_dict = {}
 
-    for d in session.drivers:
-        driver = session.get_driver(d)
-        #print(driver)
-        driver_dict[driver['FullName']] = [driver['Abbreviation'], driver['TeamColor']]
+        for d in session.drivers:
+            driver = session.get_driver(d)
+            #print(driver)
+            driver_dict[driver['FullName']] = [driver['Abbreviation'], driver['TeamColor']]
 
-    col1, col2 = st.columns(2)
-    with col1:
-        driverOne = st.selectbox('Select driver #1', driver_dict.keys())
-    
-    with col2:
-        driverTwo = st.selectbox('Select driver #2', driver_dict.keys())
+        col1, col2 = st.columns(2)
+        with col1:
+            driverOne = st.selectbox('Select driver #1', driver_dict.keys())
+        
+        with col2:
+            driverTwo = st.selectbox('Select driver #2', driver_dict.keys())
 
-    st.header("Speed")
-    fig = load_q(session, telX="Distance", telY="Speed", lblX="Distance in m", lblY="Speed in km/h", dD=driver_dict, dO=driverOne, dT=driverTwo)
-    st.plotly_chart(fig, use_container_width=True)
+        st.header("Speed")
+        fig = load_q(session, telX="Distance", telY="Speed", lblX="Distance in m", lblY="Speed in km/h", dD=driver_dict, dO=driverOne, dT=driverTwo)
+        st.plotly_chart(fig, use_container_width=True)
 
-    st.header("Gear")
-    fig = load_q(session, telX="Distance", telY="nGear", lblX="Distance in m", lblY="Gear",dD=driver_dict, dO=driverOne, dT=driverTwo)
-    st.plotly_chart(fig, use_container_width=True)
+        st.header("Gear")
+        fig = load_q(session, telX="Distance", telY="nGear", lblX="Distance in m", lblY="Gear",dD=driver_dict, dO=driverOne, dT=driverTwo)
+        st.plotly_chart(fig, use_container_width=True)
 
-    st.header("RPM")
-    fig = load_q(session, telX="Distance", telY="RPM", lblX="Distance in m", lblY="RPM",dD=driver_dict, dO=driverOne, dT=driverTwo)
-    st.plotly_chart(fig, use_container_width=True)
+        st.header("RPM")
+        fig = load_q(session, telX="Distance", telY="RPM", lblX="Distance in m", lblY="RPM",dD=driver_dict, dO=driverOne, dT=driverTwo)
+        st.plotly_chart(fig, use_container_width=True)
 
-    st.header("Throttle")
-    fig = load_q(session, telX="Distance", telY="Throttle", lblX="Distance in m", lblY="Throttle",dD=driver_dict, dO=driverOne, dT=driverTwo)
-    st.plotly_chart(fig, use_container_width=True)
+        st.header("Throttle")
+        fig = load_q(session, telX="Distance", telY="Throttle", lblX="Distance in m", lblY="Throttle",dD=driver_dict, dO=driverOne, dT=driverTwo)
+        st.plotly_chart(fig, use_container_width=True)
 
-    st.header("Brake")
-    fig = load_q(session, telX="Distance", telY="Brake", lblX="Distance in m", lblY="Brake",dD=driver_dict, dO=driverOne, dT=driverTwo)
-    st.plotly_chart(fig, use_container_width=True)
+        st.header("Brake")
+        fig = load_q(session, telX="Distance", telY="Brake", lblX="Distance in m", lblY="Brake",dD=driver_dict, dO=driverOne, dT=driverTwo)
+        st.plotly_chart(fig, use_container_width=True)
 
-    st.header("DRS")
-    fig = load_q(session, telX="Distance", telY="DRS", lblX="Distance in m", lblY="DRS",dD=driver_dict, dO=driverOne, dT=driverTwo)
-    st.plotly_chart(fig, use_container_width=True)
+        st.header("DRS")
+        fig = load_q(session, telX="Distance", telY="DRS", lblX="Distance in m", lblY="DRS",dD=driver_dict, dO=driverOne, dT=driverTwo)
+        st.plotly_chart(fig, use_container_width=True)
 
-
-
+    except fastf1.core.DataNotLoadedError:
+        st.write("error")
 
 if __name__ == '__main__':
     st.set_page_config(page_title="F1 Analysis (by matusso)", layout="wide")
 
-    col1, col2 = st.columns(2)
-    with col1:
-        selectedYear = st.selectbox('Year', [2018, 2019, 2020, 2021, 2022, 2023])
+    selectedYear = st.sidebar.selectbox(
+        "Choose year",
+        (2018, 2019, 2020, 2021, 2022, 2023)
+    )
 
     schedule = fastf1.get_event_schedule(selectedYear)
-    round_dict = {}
+    locations = []
     for index, row in schedule.iterrows():
         if row["RoundNumber"] > 0:
-            round_dict[row["Location"]] = row["RoundNumber"] 
-    
-    with col2:
-        circuitName = st.selectbox('Select Circuit', round_dict.keys())
+            locations.append(row["Location"])
 
-    print(schedule.get_event_by_round(round_dict[circuitName]))
+    # Using "with" notation
+    with st.sidebar:
+        circuitName = st.radio(
+            "Choose a Circuit",
+            (locations)
+        )
+
     st.header(str(selectedYear) + "/" + circuitName)
-
     tab1, tab2, tab3, tab4, tab5 = st.tabs(["Practice 1", "Practice 2", "Practice 3", "Qualifying", "Race"])
 
     with tab4:
